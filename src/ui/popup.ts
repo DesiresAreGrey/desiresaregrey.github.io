36bb9a7281cd7a88a50f51365874eb9f00187b8e
DesiresAreGrey <desiresaregrey@gmail.com>
2026-03-12 15:11:33 -0400

    WHY DIDNT YOU SAVE

--- src/ui/popup.ts
+++ src/ui/popup.ts
@@ -9,11 +9,11 @@ export class Popup {
     static async showPopup(content: string) {
         await this.init();
 
-        const overlay = document.createElement("div");
-        overlay.classList.add("popup-overlay");
-        overlay.style.display = "flex";
+        const popup = document.createElement("div");
+        popup.classList.add("popup-overlay");
+        popup.style.display = "flex";
 
-        overlay.innerHTML = /* html */ `
+        popup.innerHTML = /* html */ `
             <div class="popup">
                 <div class="close">
                     <svg width="28px" height="28px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="Menu / Close_SM"><path id="Vector" d="M16 16L12 12M12 12L8 8M12 12L16 8M12 12L8 16" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></g></svg>
@@ -21,22 +21,22 @@ export class Popup {
                 ${content}
             </div>
         `;
-        document.body.prepend(overlay);
-        void overlay.offsetHeight;
-        overlay.classList.add('active');
+        document.body.prepend(popup);
+        void popup.offsetHeight;
+        popup.classList.add('active');
         
-        overlay.addEventListener('click', (e) => {
-            if (e.target === overlay) {
-                overlay.classList.remove('active');
-                Utils.runAfter(() => overlay.remove(), 250, overlay);
+        popup.addEventListener('click', (e) => {
+            if (e.target === popup) {
+                popup.classList.remove('active');
+                Utils.runAfter(() => popup.remove(), 250, popup);
             }
         });
-        overlay.$('.close')?.addEventListener('click', () => {
-            overlay.classList.remove('active');
-            Utils.runAfter(() => overlay.remove(), 250, overlay);
+        popup.$('.close')?.addEventListener('click', () => {
+            popup.classList.remove('active');
+            Utils.runAfter(() => popup.remove(), 250, popup);
         });
 
-        return overlay;
+        return popup;
     }
 }
 void Popup.init();
